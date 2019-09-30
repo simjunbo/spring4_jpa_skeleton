4f1cd98ed3824d902cce1a8d1bed908670746621
simjunbo <simjunbo@tmon.co.kr>
2019-09-30 15:15:18 +0900

    spring init

---
+++ src/main/java/com/sjb/api/HomeController.java
@@ -0,0 +1,18 @@
+package com.sjb.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @RequestMapping(value = "/hello")
+    public ModelAndView helloWorld() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("hello");
+        return mv;
+    }
+}
+
